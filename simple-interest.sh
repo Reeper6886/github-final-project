868a05e0aae8eb479e52aaae2f6a1e883fe3f9c3
Reeper6886 <60592521+Reeper6886@users.noreply.github.com>
2023-07-26 11:05:50 +0800

    Update simple-interest.sh

--- simple-interest.sh
+++ simple-interest.sh
@@ -4,7 +4,7 @@
 # Do not use this in production. Sample purpose only.
 # Author: Upkar Lidder (IBM)
 # Additional Authors:
-# <your GitHub username>
+# Reeper6886
 # Input:
 # p, principal amount
 # t, time period in years
